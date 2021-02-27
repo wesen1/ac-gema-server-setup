4eb582eb510dbbf246c5c0645e1ee35c7ee760a1
wesen <wesen1@users.noreply.github.com>
2021-02-27 14:05:33 +0100

    Add util functions

---
+++ scripts/util/functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Returns a unique file path inside a given directory.
+#
+# @param string $1 The directory path to find a unique file path for
+# @param string $2 The file name prefix
+#
+getUniqueFilePath()
+{
+  directoryPath="$1"
+  fileNamePrefix="$2"
+
+  # Find a unique log backup file name
+  printf -v dateTimeString '%(%Y-%m-%d_%H:%M:%S)T\n' -1
+  fileName="$fileNamePrefix$dateTimeString"
+  uniqueFileName="$fileName"
+  counter=1
+
+  while [ -f "$directoryPath/$uniqueFileName" ]; do
+    uniqueFileName="$fileName_$counter"
+    counter=$(( $counter + 1 ))
+  done
+
+  echo "$directoryPath/$uniqueFileName"
+}
+
+export -f getUniqueFilePath
